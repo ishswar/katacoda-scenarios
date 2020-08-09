6dc4482a29b98440b4b2f578ec099be7a4f0fc44
Pranay Shah <pshah@tibco.com>
2020-08-09 14:59:21 -0700

    Added CKAD senario 1

--- ckad-senarios-1/assets/wait.sh
+++ ckad-senarios-1/assets/wait.sh
@@ -2,13 +2,13 @@
 
 show_progress()
 {
-  echo -n "Starting"
+  echo -n "Starting to install k3d CLI"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
   local temp
   while true; do
-    sudo grep -i "done" /root/katacoda-finished &> /dev/null
+    type k3d > /dev/null 2>&1
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -21,10 +21,10 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Started"
-  echo -n "Configuring"
+  echo "Done installing k3d CLI"
+  echo -n "Installing k3s cluster 1"
   while true; do
-    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
+    kubectl get pods --context=k3d-k8s
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -37,7 +37,24 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Configured"
+  echo "Done installing k3s cluster 1"
+  echo ""
+  echo -n "Installing k3s cluster 2"
+  while true; do
+    kubectl get pods --context=k3d-dk8s
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Done - all clusters are installed and configured"
 }
 
 show_progress
