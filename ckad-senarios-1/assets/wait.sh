0ebb33f85dbede700a61eabcbfd6a63f48f2e484
Pranay Shah <pshah@tibco.com>
2020-08-09 15:07:16 -0700

    Added CKAD senario 1

--- ckad-senarios-1/assets/wait.sh
+++ ckad-senarios-1/assets/wait.sh
@@ -2,7 +2,9 @@
 
 show_progress()
 {
-  echo -n "Starting to install k3d CLI"
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  echo -e  -n "Starting to install k3d CLI"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
@@ -21,8 +23,8 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Done installing k3d CLI"
-  echo -n "Installing k3s cluster 1"
+  echo -e  "${RED}Done${NC} installing k3d CLI"
+  echo -e  -n "Installing k3s cluster 1"
   while true; do
     kubectl get pods --context=k3d-k8s  > /dev/null 2>&1
     if [[ "$?" -ne 0 ]]; then
@@ -37,9 +39,8 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Done installing k3s cluster 1"
-  echo ""
-  echo -n "Installing k3s cluster 2"
+  echo -e  "${RED}Done${NC} installing k3s cluster 1"
+  echo -e  -n "Installing k3s cluster 2"
   while true; do
     kubectl get pods --context=k3d-dk8s  > /dev/null 2>&1
     if [[ "$?" -ne 0 ]]; then
@@ -54,7 +55,7 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Done - all clusters are installed and configured"
+  echo -e  "${RED}Done${NC} - all clusters are installed and configured"
 }
 
 show_progress
