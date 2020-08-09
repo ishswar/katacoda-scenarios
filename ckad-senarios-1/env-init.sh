c6c0213a12fa866cf1d32468ff178d258067390e
Pranay Shah <pshah@tibco.com>
2020-08-09 14:47:43 -0700

    Added CKAD senario 1

--- ckad-senarios-1/env-init.sh
+++ ckad-senarios-1/env-init.sh
@@ -1,1 +1,10 @@
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash - && k3d cluster create k8s -a 2 && k3d cluster create dk8s -a 1 && kubectl config use-context k3d-k8s
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash -
+
+if ( type k3d > /dev/null 2>&1 ) ; then
+  k3d cluster create k8s -a 2;
+  k3d cluster create dk8s -a 1;
+  kubectl config use-context k3d-k8s
+else
+  echo "k3d is not installed can't proceed "; exit 1;
+
+fi
