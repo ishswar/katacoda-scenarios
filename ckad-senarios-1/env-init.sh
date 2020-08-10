206b050bc5aa88dde6f36ebbd24065db5f57dab6
Pranay Shah <pshah@tibco.com>
2020-08-09 22:46:11 -0700

    Added 5 senarios

--- ckad-senarios-1/env-init.sh
+++ ckad-senarios-1/env-init.sh
@@ -8,3 +8,14 @@ else
   echo "k3d is not installed can't proceed "; exit 1;
 
 fi
+
+kubectl create ns auditing
+kubectl create ns marketing
+kubectl create cronjob -n auditing doaudits --image=busybox --schedule="*/1 * * * *" -- sleep 4
+kubectl create cronjob -n marketing sendInfo --image=busybox --schedule="*/1 * * * *" -- sleep 2
+kubectl create cronjob -n marketing sendinfo --image=busybox --schedule="*/1 * * * *" -- sleep 2
+sleep 2m
+kubectl -n marketing patch cronjobs.batch sendinfo -p '{"spec":{"suspend":true}}'
+
+# Senario 4
+kubectl create deployment deploy-charts --image=nginx:stable-alpine -n auditing
