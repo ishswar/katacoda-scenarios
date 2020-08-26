b7aa2adcc64ff5862bc63a59eb24fa0b9bdff20a
Pranay Shah <pshah@tibco.com>
2020-08-25 20:13:48 -0700

    Added k3s and Octant install scenario

--- k3s-octant-setup/assets/wait.sh
+++ k3s-octant-setup/assets/wait.sh
@@ -5,7 +5,8 @@ show_progress()
   echo "Welcome to k3s and Octant world"
   sleep 2
   clear
-  PS1='[\u@\h \W] \D{%T}\$ '
+  printf "PS1='\t [\u@\h \W]\$ '" >> ~/.bashrc
+  . ~/.bashrc
 }
 
 show_progress
