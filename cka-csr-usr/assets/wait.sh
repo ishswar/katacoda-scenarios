efb2e233cd44b9ce77ae1efe540f13eca124af88
Pranay Shah <pshah@tibco.com>
2020-09-21 01:44:53 -0700

    added CSR senario

--- cka-csr-usr/assets/wait.sh
+++ cka-csr-usr/assets/wait.sh
@@ -7,13 +7,13 @@ show_progress()
   GREEN='\033[0;32m'
   CYAN='\033[0;36m'
   NC='\033[0m' # No Color
-  echo -e  -n "Starting to install k3d CLI"
+  echo -e  -n "Upgrading kubeadm"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
   local temp
   while true; do
-    type k3d > /dev/null 2>&1
+    [ $(kubeadm version | grep 1.19 | wc -l) -eq 1 ]
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -26,10 +26,10 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo -e  "${GREEN}Done${NC} installing ${CYAN}k3d${NC} CLI"
-  echo -e  -n "Installing k3s cluster 1"
+  echo -e  "${GREEN}Done${NC} upgrading ${CYAN}kubeadm${NC}"
+  echo -e  -n "Upgrading kubelet and kubectl to 1.19"
   while true; do
-    kubectl get pods --context=k3d-k8s  > /dev/null 2>&1
+    [ $(kubectl version --client --short | grep 1.19 | wc -l) -eq 1 ]
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -42,10 +42,10 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo -e  "${GREEN}Done${NC} installing ${CYAN}k3s cluster 1${NC}"
-  echo -e  -n "Installing k3s cluster 2"
+  echo -e  "${GREEN}Done${NC} upgrading ${CYAN}kubectl to 1.19 ${NC}"
+  echo -e  -n "Upgrading kubelet to 1.19"
   while true; do
-    kubectl get pods --context=k3d-dk8s  > /dev/null 2>&1
+    [ $(kubelet --version | grep 1.19 | wc -l) -eq 1 ]
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -58,7 +58,7 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo -e  "${GREEN}Done${NC} - ${CYAN}ALL${NC} clusters are installed and configured"
+  echo -e  "${GREEN}Done${NC} - ${CYAN}ALL${NC} upgrading kubeadm,kubectl and kubelet"
   sleep 1
   clear
 }
