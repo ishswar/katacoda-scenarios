49be403958c9854e75d5e311de01f60491468eea
Pranay Shah <pshah@tibco.com>
2020-10-03 15:32:16 -0700

    Updated HA senarios

--- cka-ha-control-planes/env-init.sh
+++ cka-ha-control-planes/env-init.sh
@@ -15,6 +15,7 @@ ssh node01 apt-get install -y kubeadm=1.19.0-00
 ssh node01 apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
 ssh node01 kubeadm config images pull
 ssh node01 systemctl disable kubelet
+ssh node01 systemctl stop kubelet
 
 ETCD_CTCL_VERSION=3.4.3
 wget -q https://github.com/etcd-io/etcd/releases/download/v${ETCD_CTCL_VERSION}/etcd-v${ETCD_CTCL_VERSION}-linux-amd64.tar.gz
