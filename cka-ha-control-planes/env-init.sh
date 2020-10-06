5e51d9d5a85ed74776a21ac9d40dfb90c001d8dd
Pranay Shah <pshah@tibco.com>
2020-10-06 15:20:50 -0700

    Updated HA senarios

--- cka-ha-control-planes/env-init.sh
+++ cka-ha-control-planes/env-init.sh
@@ -8,6 +8,18 @@ apt-get update && apt-get install -y kubeadm=1.19.0-00
 apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
 kubeadm config images pull
 
+#git clone https://github.com/ishswar/k8s-scenarios.git
+
+ssh node01 apt-get update
+ssh node01 apt-get install -y kubeadm=1.19.0-00
+ssh node01 apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
+ssh node01 kubeadm config images pull
+ssh node01 systemctl stop kubelet
+ssh node01 systemctl disable kubelet
+ssh node01 systemctl status kubelet >> ser.txt
+echo "========" >> ser.text
+ssh node01 systemctl list-unit-files --type=service | grep kubelet >> ser.txt
+
 ETCD_CTCL_VERSION=3.4.3
 wget -q https://github.com/etcd-io/etcd/releases/download/v${ETCD_CTCL_VERSION}/etcd-v${ETCD_CTCL_VERSION}-linux-amd64.tar.gz
 tar -zxf etcd-v${ETCD_CTCL_VERSION}-linux-amd64.tar.gz
@@ -20,24 +32,3 @@ echo "export RED='\033[0;31m'" >> ~/.bashrc
 echo "export YELLOW='\033[1;33m'" >> ~/.bashrc
 echo "export CYAN='\033[0;36m'" >> ~/.bashrc
 echo "export NC='\033[0m' # No Color" >> ~/.bashrc
-
-#git clone https://github.com/ishswar/k8s-scenarios.git
-ssh node01 apt-get update
-ssh node01 apt-get install -y kubeadm=1.19.0-00
-ssh node01 apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
-ssh node01 kubeadm config images pull
-
-ssh node01 systemctl stop kubelet
-ssh node01 systemctl disable kubelet
-ssh node01 systemctl status kubelet >> ser.txt
-echo "========" >> ser.txt
-ssh node01 systemctl list-unit-files --type=service | grep kubelet >> ser.txt
-
-ETCD_CTCL_VERSION=3.4.3
-
-ssh node01 wget -q https://github.com/etcd-io/etcd/releases/download/v3.4.3/etcd-v3.4.3-linux-amd64.tar.gz
-ssh node01 tar -zxf etcd-v3.4.3-linux-amd64.tar.gz
-ssh node01 cd etcd-v3.4.3-linux-amd64
-ssh node01 sudo cp etcdctl /usr/local/bin
-
-
